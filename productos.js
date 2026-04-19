f50a380824a004ebbe7b2c53583aca6dfa8851a6
sbkstore <sbkstore458@gmail.com>
2026-04-19 20:28:26 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -5,10 +5,5 @@ const productos = {
     { titulo: "DOMINIO + HOSTING", precio: "$150.000", etiqueta: "x5 años", descripcion: "Dominio + Hosting X 5 años.", imagenes: ["plantas/fotos/3.jpg"] },
     { titulo: "Pagos al 50%", precio: "$199.000", etiqueta: "50%", descripcion: "Múltiples bolsillos, impermeable.", imagenes: ["plantas/fotos/4.jpg"] },
     { titulo: "infoexperto", precio: "$85.000", etiqueta: "400 TOKENS", descripcion: "BOT DE HERRAMIENTA PROFESIONAL.", imagenes: ["plantas/fotos/5.jpg"] },
-    { titulo: "Zapatillas Jordan 1", precio: "$120.000", etiqueta: "TOP", descripcion: "Edición limitada.", imagenes: ["plantas/fotos/6.jpg"] },
-    { titulo: "Conjunto Trap Star", precio: "$85.000", etiqueta: "OFERTA", descripcion: "Estilo Londres.", imagenes: ["plantas/fotos/7.jpg"] },
-    { titulo: "Media Jordan Crew", precio: "$8.000", etiqueta: "", descripcion: "Pack x3 pares.", imagenes: ["plantas/fotos/8.jpg"] },
-    { titulo: "Riñonera Tactical", precio: "$20.000", etiqueta: "NUEVO", descripcion: "Correas reforzadas.", imagenes: ["plantas/fotos/9.jpg"] },
-    { titulo: "Piluso Street", precio: "$12.000", etiqueta: "HOT", descripcion: "Ideal para el verano.", imagenes: ["plantas/fotos/10.jpg"] }
-  ]
+    ]
 };
